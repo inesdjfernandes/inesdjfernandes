0f4389bb1ce465e5d935d2b515239bd90f73addb
inesfernandes <inesdejesusfernandes0@gmail.com>
2023-11-18 16:58:33 +0000

    alterei ficheiro paises commit na branch

--- paises.js
+++ paises.js
@@ -3,8 +3,8 @@ var Schema       = mongoose.Schema;
 
 var PaisesSchema   = new Schema({
     cod_pais: {type: String, nullable:false},
-    nome_pais: {type: String, nullable:false},
     cod_zonageo: {type: Number, nullable:false} //será number/string??
 });
+/////
 
 module.exports = mongoose.model('paises', PaisesSchema);
