e55035055a3cc8ea7cac6a8fc0d163952b7707c3
inesfernandes <inesdejesusfernandes0@gmail.com>
2023-11-18 17:16:24 +0000

    adicionei novo file e alterei o outro

--- paises.js
+++ paises.js
@@ -10,9 +10,7 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var ZonaSchema   = new Schema({
-    cod_zonageo: {type: Number, nullable:false}, //será number/string??
-    nome_zonageo: {type: String, nullable:false}    
 });
-
+///
 module.exports = mongoose.model('zona', ZonaSchema);
 module.exports = mongoose.model('paises', PaisesSchema);
